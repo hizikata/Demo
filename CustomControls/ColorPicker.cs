017a0b771b9bc00b4e59da166c1b05bd63762e5d
hizikata <35916025+hizikata@users.noreply.github.com>
2018-07-30 23:02:23 +0800

    add wpf custom control 180730

---
+++ CustomControls/ColorPicker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CustomControls
+{
+    public class ColorPicker:System.Windows.Controls.UserControl
+    {
+        public static DependencyProperty ColorProperty;
+        public static DependencyProperty RedProperty;
+        public static DependencyProperty GreenProperty;
+        public static DependencyProperty BlueProperty;
+        static ColorPicker()
+        {
+            ColorProperty = DependencyProperty.Register("Color", typeof(Color), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(Colors.Black, new PropertyChangedCallback(OnColorChanged)));
+            RedProperty = DependencyProperty.Register("Red", typeof(byte), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            GreenProperty = DependencyProperty.Register("Green", typeof(byte), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            BlueProperty = DependencyProperty.Register("Blue", typeof(byte), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+        }
+        public Color Color
+        {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+        public static void OnColorRGBChanged(DependencyObject sender,DependencyPropertyChangedEventArgs e)
+        {
+            ColorPicker colorPicker = (ColorPicker)sender;
+
+        }
+        public static void OnColorChanged(DependencyObject sender,DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+    }
+}
